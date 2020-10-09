963bd1c03ac36a81da7095694576f17d946cf96e
James Gangur <james.gangur@gmail.com>
2020-10-09 20:56:00 +1100

    fix opengl function loading on windows

--- source/sqee/gl/Loader.cpp
+++ source/sqee/gl/Loader.cpp
@@ -4,256 +4,13 @@
 
 //============================================================================//
 
-/*
-static void* get_proc(const char *namez);
-
-#ifdef SQEE_WINDOWS
-
-#include <windows.h>
-static HMODULE libGL;
-
-typedef void* (APIENTRYP PFNWGLGETPROCADDRESSPROC_PRIVATE)(const char*);
-static PFNWGLGETPROCADDRESSPROC_PRIVATE gladGetProcAddressPtr;
-
-#ifdef _MSC_VER
-#ifdef __has_include
-  #if __has_include(<winapifamily.h>)
-    #define HAVE_WINAPIFAMILY 1
-  #endif
-#elif _MSC_VER >= 1700 && !_USING_V110_SDK71_
-  #define HAVE_WINAPIFAMILY 1
-#endif
+#if defined(SQEE_LINUX)
+  #include <GL/glx.h>
+#elif defined(SQEE_WINDOWS)
+  #include <windows.h>
 #endif
 
-#ifdef HAVE_WINAPIFAMILY
-  #include <winapifamily.h>
-  #if !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) && WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP)
-    #define IS_UWP 1
-  #endif
-#endif
-
-static
-int open_gl(void) {
-#ifndef IS_UWP
-    libGL = LoadLibraryW(L"opengl32.dll"));
-    if(libGL != NULL) {
-        void (* tmp)(void);
-        tmp = (void(*)(void)) GetProcAddress(libGL, "wglGetProcAddress"));
-        gladGetProcAddressPtr = (PFNWGLGETPROCADDRESSPROC_PRIVATE) tmp;
-        return gladGetProcAddressPtr != NULL;
-    }
-#endif
-
-    return 0;
-}
-
-static
-void close_gl(void) {
-    if(libGL != NULL) {
-        FreeLibrary((HMODULE) libGL);
-        libGL = NULL;
-    }
-}
-#else
-#include <dlfcn.h>
-static void* libGL;
-
-#if !defined(__APPLE__) && !defined(__HAIKU__)
-typedef void* (APIENTRYP PFNGLXGETPROCADDRESSPROC_PRIVATE)(const char*);
-static PFNGLXGETPROCADDRESSPROC_PRIVATE gladGetProcAddressPtr;
-#endif
-
-static
-int open_gl(void) {
-#ifdef __APPLE__
-    static const char *NAMES[] = {
-        "../Frameworks/OpenGL.framework/OpenGL",
-        "/Library/Frameworks/OpenGL.framework/OpenGL",
-        "/System/Library/Frameworks/OpenGL.framework/OpenGL",
-        "/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL"
-    };
-#else
-    static const char *NAMES[] = {"libGL.so.1", "libGL.so"};
-#endif
-
-    unsigned int index = 0;
-    for(index = 0; index < (sizeof(NAMES) / sizeof(NAMES[0])); index++) {
-        libGL = dlopen(NAMES[index], RTLD_NOW | RTLD_GLOBAL);
-
-        if(libGL != NULL) {
-#if defined(__APPLE__) || defined(__HAIKU__)
-            return 1;
-#else
-            gladGetProcAddressPtr = (PFNGLXGETPROCADDRESSPROC_PRIVATE)dlsym(libGL,
-                "glXGetProcAddressARB"));
-            return gladGetProcAddressPtr != NULL;
-#endif
-        }
-    }
-
-    return 0;
-}
-
-static
-void close_gl(void) {
-    if(libGL != NULL) {
-        dlclose(libGL);
-        libGL = NULL;
-    }
-}
-#endif
-
-static
-void* get_proc(const char *namez) {
-    void* result = NULL;
-    if(libGL == NULL) return NULL;
-
-#if !defined(__APPLE__) && !defined(__HAIKU__)
-    if(gladGetProcAddressPtr != NULL) {
-        result = gladGetProcAddressPtr(namez);
-    }
-#endif
-    if(result == NULL) {
-#if defined(_WIN32) || defined(__CYGWIN__)
-        result = (void*)GetProcAddress((HMODULE) libGL, namez);
-#else
-        result = dlsym(libGL, namez);
-#endif
-    }
-
-    return result;
-}
-
-int gladLoadGL(void) {
-    int status = 0;
-
-    if(open_gl()) {
-        status = gladLoadGLLoader(&get_proc);
-        close_gl();
-    }
-
-    return status;
-}
-
-struct gladGLversionStruct GLVersion = { 0, 0 };
-
-#if defined(GL_ES_VERSION_3_0) || defined(GL_VERSION_3_0)
-#define _GLAD_IS_SOME_NEW_VERSION 1
-#endif
-
-static int max_loaded_major;
-static int max_loaded_minor;
-
-static const char *exts = NULL;
-static int num_exts_i = 0;
-static char **exts_i = NULL;
-
-static int get_exts(void) {
-#ifdef _GLAD_IS_SOME_NEW_VERSION
-    if(max_loaded_major < 3) {
-#endif
-        exts = (const char *)glGetString(GL_EXTENSIONS);
-#ifdef _GLAD_IS_SOME_NEW_VERSION
-    } else {
-        unsigned int index;
-
-        num_exts_i = 0;
-        glGetIntegerv(GL_NUM_EXTENSIONS, &num_exts_i);
-        if (num_exts_i > 0) {
-            exts_i = (char **)malloc((size_t)num_exts_i * (sizeof *exts_i));
-        }
-
-        if (exts_i == NULL) {
-            return 0;
-        }
-
-        for(index = 0; index < (unsigned)num_exts_i; index++) {
-            const char *gl_str_tmp = (const char*)glGetStringi(GL_EXTENSIONS, index);
-            size_t len = strlen(gl_str_tmp);
-
-            char *local_str = (char*)malloc((len+1) * sizeof(char));
-            if(local_str != NULL) {
-                memcpy(local_str, gl_str_tmp, (len+1) * sizeof(char));
-            }
-            exts_i[index] = local_str;
-        }
-    }
-#endif
-    return 1;
-}
-
-static void free_exts(void) {
-    if (exts_i != NULL) {
-        int index;
-        for(index = 0; index < num_exts_i; index++) {
-            free((char *)exts_i[index]);
-        }
-        free((void *)exts_i);
-        exts_i = NULL;
-    }
-}
-
-static int has_ext(const char *ext) {
-#ifdef _GLAD_IS_SOME_NEW_VERSION
-    if(max_loaded_major < 3) {
-#endif
-        const char *extensions;
-        const char *loc;
-        const char *terminator;
-        extensions = exts;
-        if(extensions == NULL || ext == NULL) {
-            return 0;
-        }
-
-        while(1) {
-            loc = strstr(extensions, ext);
-            if(loc == NULL) {
-                return 0;
-            }
-
-            terminator = loc + strlen(ext);
-            if((loc == extensions || *(loc - 1) == ' ') &&
-                (*terminator == ' ' || *terminator == '\0')) {
-                return 1;
-            }
-            extensions = terminator;
-        }
-#ifdef _GLAD_IS_SOME_NEW_VERSION
-    } else {
-        int index;
-        if(exts_i == NULL) return 0;
-        for(index = 0; index < num_exts_i; index++) {
-            const char *e = exts_i[index];
-
-            if(exts_i[index] != NULL && strcmp(e, ext) == 0) {
-                return 1;
-            }
-        }
-    }
-#endif
-
-    return 0;
-}*/
-
-/*
-int glad_GL_VERSION_1_0 = reinterpret_cast<decltype(glad_GL_VERSION_1_0)>(load("GL_VERSION_1_0"));
-int glad_GL_VERSION_1_1 = reinterpret_cast<decltype(glad_GL_VERSION_1_1)>(load("GL_VERSION_1_1"));
-int glad_GL_VERSION_1_2 = reinterpret_cast<decltype(glad_GL_VERSION_1_2)>(load("GL_VERSION_1_2"));
-int glad_GL_VERSION_1_3 = reinterpret_cast<decltype(glad_GL_VERSION_1_3)>(load("GL_VERSION_1_3"));
-int glad_GL_VERSION_1_4 = reinterpret_cast<decltype(glad_GL_VERSION_1_4)>(load("GL_VERSION_1_4"));
-int glad_GL_VERSION_1_5 = reinterpret_cast<decltype(glad_GL_VERSION_1_5)>(load("GL_VERSION_1_5"));
-int glad_GL_VERSION_2_0 = reinterpret_cast<decltype(glad_GL_VERSION_2_0)>(load("GL_VERSION_2_0"));
-int glad_GL_VERSION_2_1 = reinterpret_cast<decltype(glad_GL_VERSION_2_1)>(load("GL_VERSION_2_1"));
-int glad_GL_VERSION_3_0 = reinterpret_cast<decltype(glad_GL_VERSION_3_0)>(load("GL_VERSION_3_0"));
-int glad_GL_VERSION_3_1 = reinterpret_cast<decltype(glad_GL_VERSION_3_1)>(load("GL_VERSION_3_1"));
-int glad_GL_VERSION_3_2 = reinterpret_cast<decltype(glad_GL_VERSION_3_2)>(load("GL_VERSION_3_2"));
-int glad_GL_VERSION_3_3 = reinterpret_cast<decltype(glad_GL_VERSION_3_3)>(load("GL_VERSION_3_3"));
-int glad_GL_VERSION_4_0 = reinterpret_cast<decltype(glad_GL_VERSION_4_0)>(load("GL_VERSION_4_0"));
-int glad_GL_VERSION_4_1 = reinterpret_cast<decltype(glad_GL_VERSION_4_1)>(load("GL_VERSION_4_1"));
-int glad_GL_VERSION_4_2 = reinterpret_cast<decltype(glad_GL_VERSION_4_2)>(load("GL_VERSION_4_2"));
-int glad_GL_VERSION_4_3 = reinterpret_cast<decltype(glad_GL_VERSION_4_3)>(load("GL_VERSION_4_3"));
-int glad_GL_VERSION_4_4 = reinterpret_cast<decltype(glad_GL_VERSION_4_4)>(load("GL_VERSION_4_4"));
-int glad_GL_VERSION_4_5 = reinterpret_cast<decltype(glad_GL_VERSION_4_5)>(load("GL_VERSION_4_5"));*/
+//============================================================================//
 
 namespace sq::gl::detail {
 
@@ -957,46 +714,40 @@ decltype(glWaitSync) glWaitSync = nullptr;
 
 //============================================================================//
 
-#if defined(SQEE_LINUX)
-
-#include <GL/glx.h>
-
-#define LOAD_FUNCTION(Version, Name) \
-  gl::detail::Name = reinterpret_cast<decltype(gl::detail::Name)>(glXGetProcAddress(reinterpret_cast<const GLubyte*>(#Name))); \
-  if (gl::detail::Name == nullptr) return "GLX: could not load version " #Version;
-
-#elif defined(SQEE_WINDOWS)
-
-#include <windows.h>
-
-#define LOAD_FUNCTION(Version, Name) \
-  gl::detail::Name = reinterpret_cast<decltype(gl::detail::Name)>(wglGetProcAddress(#Name)); \
-  if (gl::detail::Name == nullptr) return "WGL: could not load version " #Version;
-
-#endif
-
-//============================================================================//
+#include <sqee/debug/Logging.hpp>
 
 bool sq::load_opengl_core45_functions()
 {
-    /*#ifdef SQEE_WINDOWS
+  #if defined(SQEE_LINUX)
 
-    // open
+    #define LOAD_FUNCTION(Version, Name) \
+    { \
+        void (*funcPtr)() = glXGetProcAddress(reinterpret_cast<const GLubyte*>(#Name)); \
+        if (funcPtr) gl::detail::Name = reinterpret_cast<decltype(gl::detail::Name)>(funcPtr); \
+        else return false; \
+    }
 
-//    HMODULE gl32dll = LoadLibraryA("opengl32.dll");
-//    if (gl32dll == NULL) return "could not load opengl32.dll";
+  #elif defined(SQEE_WINDOWS)
 
-    const auto load = [&](const char* name) -> void(*)()
-    {
-        auto result = wglGetProcAddress(name);
+    HMODULE gl32dll = LoadLibraryA("opengl32.dll");
+    if (gl32dll == NULL) return false;
 
-    };
+    #define LOAD_FUNCTION(Version, Name) \
+    sq::log_info("attempting to load '{}' from version '{}'", #Name, #Version); \
+    if constexpr (Version > 1.1) \
+    { \
+        PROC funcPtr = wglGetProcAddress(#Name); \
+        if (funcPtr) gl::detail::Name = reinterpret_cast<decltype(gl::detail::Name)>(funcPtr); \
+        else return false; \
+    } \
+    else \
+    { \
+        FARPROC funcPtr = GetProcAddress(gl32dll, #Name); \
+        if (funcPtr) gl::detail::Name = reinterpret_cast<decltype(gl::detail::Name)>(funcPtr); \
+        else return false; \
+    }
 
-    // close
-
-//    FreeLibrary(HMODULE gl32dll);
-//    gl32dll = NULL;
-    #endif*/
+  #endif
 
     LOAD_FUNCTION(1.0, glCullFace)
     LOAD_FUNCTION(1.0, glFrontFace)
@@ -1580,7 +1331,7 @@ bool sq::load_opengl_core45_functions()
     LOAD_FUNCTION(4.3, glGetObjectLabel)
     LOAD_FUNCTION(4.3, glObjectPtrLabel)
     LOAD_FUNCTION(4.3, glGetObjectPtrLabel)
-    LOAD_FUNCTION(4.3, glGetPointerv)
+    LOAD_FUNCTION(1.1, glGetPointerv) // actually from core 4.3, but name is from 1.1
 
     LOAD_FUNCTION(4.4, glBufferStorage)
     LOAD_FUNCTION(4.4, glClearTexImage)
@@ -1715,58 +1466,9 @@ bool sq::load_opengl_core45_functions()
     LOAD_FUNCTION(4.5, glGetnMinmax)
     LOAD_FUNCTION(4.5, glTextureBarrier)
 
+  #ifdef SQEE_WINDOWS
+    FreeLibrary(gl32dll);
+  #endif
+
     return true; // hooray!
 }
-
-//static void find_coreGL()
-//{
-//    const char* const version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
-//    if (version == nullptr) return;
-
-//    int major, minor;
-
-//    #ifdef _MSC_VER
-//      sscanf_s(version, "%d.%d", &major, &minor);
-//    #else
-//      sscanf(version, "%d.%d", &major, &minor);
-//    #endif
-
-//    GLVersion.major = major;
-//    GLVersion.minor = minor;
-
-//    if (GLVersion.major > 4 || (GLVersion.major >= 4 && GLVersion.minor >= 5))
-//    {
-//        max_loaded_major = 4;
-//        max_loaded_minor = 5;
-//    }
-//}
-
-//int gladLoadGLLoader(GLADloadproc load)
-//{
-//    GLVersion.major = 0; GLVersion.minor = 0;
-//    glGetString = (PFNGLGETSTRINGPROC)load("glGetString"));
-//    if(glGetString == NULL) return 0;
-//    if(glGetString(GL_VERSION) == NULL) return 0;
-//    find_coreGL();
-//    load_GL_VERSION_1_0(load);
-//    load_GL_VERSION_1_1(load);
-//    load_GL_VERSION_1_2(load);
-//    load_GL_VERSION_1_3(load);
-//    load_GL_VERSION_1_4(load);
-//    load_GL_VERSION_1_5(load);
-//    load_GL_VERSION_2_0(load);
-//    load_GL_VERSION_2_1(load);
-//    load_GL_VERSION_3_0(load);
-//    load_GL_VERSION_3_1(load);
-//    load_GL_VERSION_3_2(load);
-//    load_GL_VERSION_3_3(load);
-//    load_GL_VERSION_4_0(load);
-//    load_GL_VERSION_4_1(load);
-//    load_GL_VERSION_4_2(load);
-//    load_GL_VERSION_4_3(load);
-//    load_GL_VERSION_4_4(load);
-//    load_GL_VERSION_4_5(load);
-
-//    if (!find_extensionsGL()) return 0;
-//    return GLVersion.major != 0 || GLVersion.minor != 0;
-//}
